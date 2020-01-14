9dd7adf00094d28d278677f33083f2e17fb1cfc4
Shivam Tiwari <38368497+variiable@users.noreply.github.com>
2020-01-14 23:34:36 +0530

    basic build

---
+++ assets/scripts/app.js
@@ -0,0 +1,12 @@
+ const defaultValue = 0;
+ let currentResult = defaultValue;
+
+ currentResult = currentResult + 50;
+
+ let calculationDescription = '(' + defaultValue + '+10)';
+
+ outputResult(currentResult, calculationDescription);
+
+ //1:38:00
+ 
+
