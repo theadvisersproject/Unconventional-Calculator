5f7df7e30ec2061a27ce38e6e4b79fe8e0cf7193
Shivam Tiwari <38368497+variiable@users.noreply.github.com>
2020-01-15 22:16:54 +0530

    Update addBtn

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,15 @@
  const defaultValue = 0;
  let currentResult = defaultValue;
 
+ function getUserNumberInput(){
+   return parseInt(userInput.value);
+ }
+ 
+ 
  function add(){
-   const calcDescription = `${currentResult} + ${userInput.value}`;
-   currentResult = currentResult + parseInt(userInput.value);//memorization: .toString() used to convert number into a string.
+   const enteredNumber = getUserNumberInput();
+   const calcDescription = `${currentResult} + ${enteredNumber}`;
+   currentResult = currentResult + enteredNumber;
    outputResult(currentResult, calcDescription);
  }
  
