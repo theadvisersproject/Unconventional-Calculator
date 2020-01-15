830eefc400dab82bd26915fcf411e4a337ccc75f
Shivam Tiwari <38368497+variiable@users.noreply.github.com>
2020-01-15 22:22:15 +0530

    removed code repitions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,14 +4,31 @@
  function getUserNumberInput(){
    return parseInt(userInput.value);
  }
+
+ function createAndWriteLog(operators, resultBeforeCalc, calcNumber){
+   const calcDescription = `${resultBeforeCalc} ${operators} ${calcNumber}`;
+
+ }
  
  
  function add(){
    const enteredNumber = getUserNumberInput();
-   const calcDescription = `${currentResult} + ${enteredNumber}`;
+   
    currentResult = currentResult + enteredNumber;
    outputResult(currentResult, calcDescription);
  }
+
+ function substract(){
+   const enteredNumber = getUserNumberInput();
+   const calcDescription = `${currentResult} - ${enteredNumber}`;
+   currentResult = currentResult - enteredNumber;
+   outputResult(currentResult, calcDescription);
+ }
+
+ function multiply(){}
+
+ function divide() {}
+
  
  addBtn.addEventListener('click', add);
 
